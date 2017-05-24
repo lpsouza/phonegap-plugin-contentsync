8a579a0e94e2e4b60a157116ecbbad2137354b16
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2017-05-23 21:32:39 -0300

    Test unzipSync (signature in JAVA)

--- src/android/Sync.java
+++ src/android/Sync.java
@@ -866,41 +866,44 @@ public class Sync extends CordovaPlugin {
         try {
             Log.d(LOG_TAG, "Try test signature");
 
-            // Public Key
-            InputStream keyfis = webView.getContext().getAssets().open("app.crt");
+            /* Public Key */
+            InputStream keyfis = webView.getContext().getAssets().open("app.pub");
+            // FileInputStream keyfis = new FileInputStream("..\\phonegap-plugin-contentsync\\src\\android\\app.pub");
             byte[] encKey = new byte[keyfis.available()];  
             keyfis.read(encKey);
 
             keyfis.close();
+
             X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(encKey);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            KeyFactory keyFactory = KeyFactory.getInstance("DSA", "SUN");
             PublicKey pubKey = keyFactory.generatePublic(pubKeySpec);
 
-            // Signature
+            /* signature */
             InputStream sigfis = new URL("https://contentsync-pf.azurewebsites.net/www.zip.sig").openStream();
+            // FileInputStream sigfis = new FileInputStream("..\\updater\\www.zip.sig");
             byte[] sigToVerify = new byte[sigfis.available()]; 
             sigfis.read(sigToVerify);
             sigfis.close();
 
-            Signature sig = Signature.getInstance("SHA256withRSA");
-
+            Signature sig = Signature.getInstance("SHA1withDSA", "SUN");
             sig.initVerify(pubKey);
 
-            // Zip file
+            /* File */
             FileInputStream datafis = new FileInputStream(targetFile.getAbsolutePath());
+            // FileInputStream datafis = new FileInputStream("..\\updater\\www.zip");
             BufferedInputStream bufin = new BufferedInputStream(datafis);
 
-            // byte[] buffer = new byte[1024];
-            // int len;
-            // while (bufin.available() != 0) {
-            //     len = bufin.read(buffer);
-            //     sig.update(buffer, 0, len);
-            // };
+            byte[] buffer = new byte[1024];
+            int len;
+            while (bufin.available() != 0) {
+                len = bufin.read(buffer);
+                sig.update(buffer, 0, len);
+            };
 
-            // bufin.close();
+            bufin.close();
 
-            // boolean verifies = sig.verify(sigToVerify);
-            // Log.e(LOG_TAG, "Signature verifies: " + verifies);
+            boolean verifies = sig.verify(sigToVerify);
+            Log.e(LOG_TAG, "Signature verifies: " + verifies);
 
         } catch (Exception e) {
             Log.e(LOG_TAG, "Zip validation signature error: " + e.toString());
